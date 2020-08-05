85d76583fbd3ce0bd345a7cd837ab9e9f3909cba
LeeJaeHyun <myleje@gmail.com>
2020-08-05 16:32:08 +0900

    Project Upload

---
+++ Assets/Scripts/ViveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+            SteamVR_Actions.{ActionSet}_{BindMapping}.GetStateDown({Controller})
+            GetStateDown()
+            GetState()
+            GetStateUp();
+        */
+        if (SteamVR_Actions.default_InteractUI.GetStateDown(SteamVR_Input_Sources.LeftHand))
+        {
+            Debug.Log("Trigger Left Hand");
+        }
+    }
+}
