1f17a62d75b7b019943e93d21a5f35684820cc9c
LeeJaeHyun <myleje@gmail.com>
2020-08-06 13:08:46 +0900

    .

--- Assets/02.Scripts/LaserPointer.cs
+++ Assets/02.Scripts/LaserPointer.cs
@@ -10,17 +10,19 @@ public class LaserPointer : MonoBehaviour
     private SteamVR_Action_Boolean trigger;
 
     private LineRenderer line;
-    
+    public float maxDistance = 10.0f;
+    public Color defaultColor = Color.green; //new Color(0.0f, 1.0f, 0.0f, 0.0f);
+    public Color clickedColor = Color.red;
 
     void Start()
     {
         pose = GetComponent<SteamVR_Behaviour_Pose>();    
-        hand = pose.inputSource;    
+        hand = pose.inputSource;
+        CreateLine();  
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateLine()
     {
-        
+        line = this.gameObject.AddComponent<LineRenderer>();
     }
 }
