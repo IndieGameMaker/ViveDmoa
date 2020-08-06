a73bcebd209b7c2399e9cd4da85666fae307b0de
LeeJaeHyun <myleje@gmail.com>
2020-08-06 18:05:24 +0900

    .

--- Assets/02.Scripts/GrabManager.cs
+++ Assets/02.Scripts/GrabManager.cs
@@ -7,6 +7,7 @@ public class GrabManager : MonoBehaviour
 {
     private SteamVR_Action_Boolean grab;
     private SteamVR_Input_Sources hand;
+    private SteamVR_Action_Pose pose;
 
     private Transform grabObject;
     private bool isTouched;
@@ -16,6 +17,7 @@ public class GrabManager : MonoBehaviour
     {
         grab = SteamVR_Actions.default_GrabGrip;
         hand = SteamVR_Input_Sources.Any;
+        pose = SteamVR_Actions.default_Pose;
     }
 
     // Update is called once per frame
@@ -23,11 +25,14 @@ public class GrabManager : MonoBehaviour
     {
         if (isTouched == true && grab.GetStateDown(hand))
         {
+            grabObject.GetComponent<Rigidbody>().isKinematic = true;
             grabObject.SetParent(this.transform);
         }
         if (isTouched == true && grab.GetStateUp(hand))
         {
             grabObject.SetParent(null);
+            grabObject.GetComponent<Rigidbody>().isKinematic = false;
+            grabObject.GetComponent<Rigidbody>().velocity = pose.GetLastVelocity(hand);
             isTouched = false;
             grabObject = null;
         }
