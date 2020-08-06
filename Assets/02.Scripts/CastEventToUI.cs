05f5b9d9f338c3bf2ee30a3fa12b83cdabf34fd1
LeeJaeHyun <myleje@gmail.com>
2020-08-06 11:47:59 +0900

    .

---
+++ Assets/02.Scripts/CastEventToUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.Extras;
+
+public class CastEventToUI : MonoBehaviour
+{
+    private SteamVR_LaserPointer laserPointer;
+
+    void Awake()
+    {
+        laserPointer = GetComponent<SteamVR_LaserPointer>();        
+    }
+
+    
+    //Laser Pointer Hover
+    void OnPointerEnter(object sender, PointerEventArgs e)
+    {
+
+    }
+
+    //Laser Pointer Exit
+    void OnPointerExit(object sender, PointerEventArgs e)
+    {
+
+    }
+
+    //Laser Pointer Click
+    void OnPointerClick(object sender, PointerEventArgs e)
+    {
+
+    }
+
+}
