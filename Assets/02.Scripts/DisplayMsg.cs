c71854ed6bf48dcc8bd9ae2aa1e61886dcf2e5c0
LeeJaeHyun <myleje@gmail.com>
2020-08-06 12:33:33 +0900

    .

---
+++ Assets/02.Scripts/DisplayMsg.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayMsg : MonoBehaviour
+{
+    private Text msgText;
+
+    void Start()
+    {
+        msgText = GetComponent<Text>();        
+    }
+
+    public void Message(string msg)
+    {
+        msgText.text = msg;
+    }
+}
