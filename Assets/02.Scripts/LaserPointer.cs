6c372712219dc8ac07335d2cdfed2e3eb5af715c
LeeJaeHyun <myleje@gmail.com>
2020-08-06 13:04:04 +0900

    .

---
+++ Assets/02.Scripts/LaserPointer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LaserPointer : MonoBehaviour
+{
+    private SteamVR_Behaviour_Pose pose;
+    private SteamVR_Input_Sources hand;
+    private SteamVR_Action_Boolean trigger;
+
+    private LineRenderer line;
+    
+
+    void Start()
+    {
+        pose = GetComponent<SteamVR_Behaviour_Pose>();    
+        hand = pose.inputSource;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
